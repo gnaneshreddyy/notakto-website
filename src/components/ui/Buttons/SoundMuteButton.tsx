78369c713f171fc999b499879f00a9bcb002f71c
gnaneshreddyy <gnaneshreddyy@gmail.com>
2025-10-20 20:36:28 +0530

    post reveiw

--- src/components/ui/Buttons/SoundMuteButton.tsx
+++ src/components/ui/Buttons/SoundMuteButton.tsx
@@ -5,10 +5,10 @@ export function SoundMuteButton({
 }: ButtonHTMLAttributes<HTMLButtonElement>) {
 	return (
 		<button
-			className={
-				"bg-blue-600 hover:bg-blue-700 text-white px-3 py-2 w-20 text-lg"
-			}
 			{...props}
+			className={
+				"bg-blue-600 hover:bg-blue-700 text-white px-3 py-2 w-17 text-lg"
+			}
 		/>
 	);
 }
